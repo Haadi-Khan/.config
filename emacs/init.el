132d15b9d76d58d871d0dc9e60e1053094484b82
Haadi Khan <haadikhan05@gmail.com>
2022-04-22 11:13:02 -0400

    DAP Mode

--- emacs/init.el
+++ emacs/init.el
@@ -7,11 +7,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("171d1ae90e46978eb9c342be6658d937a83aaa45997b1d7af7657546cae5985b" default))
- '(package-selected-packages
-   '(real-auto-save clang-format+ clang-format subed xah-fly-keys modern-cpp-font-lock atom-one-dark-theme company-lsp lsp-python-ms ccls undo-tree org-edit-latex org-fragtog org-bullets org-evil highlight-indent-guides flyspell-popup centaur-tabs lsp-java markdown-mode magit LaTeX-mode writeroom-mode perspective neotree evil-tutor elfeed-goodies sudo-edit ox-man dired smex org-tempo all-the-icons-dired which-key vterm use-package toc-org projectile peep-dired magit-todos magit-lfs ivy-rich ivy-posframe haskell-mode general evil-collection eshell-syntax-highlighting doom-themes doom-modeline dired-open dashboard counsel))
- '(persp-mode t))
+ '(warning-suppress-types '((use-package) (use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
