356fa4563dafb69c59ffc92676d7e532b92a6019
Haadi Khan <haadikhan05@gmail.com>
2022-06-26 01:51:24 -0400

    Updated with new font

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,8 +6,16 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar haadibar 2>&1 | tee -a /tmp/polybar1.log & disown
+#echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+#polybar haadibar 2>&1 | tee -a /tmp/polybar1.log & disown
 #polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload haadibar &
+  done
+else
+  polybar --reload haadibar &
+fi
 
 echo "Bars launched..."
