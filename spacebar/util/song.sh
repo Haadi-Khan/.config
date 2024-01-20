e6ff83f5d4f14f594a41ff2e1abf4780003156cf
Haadi Khan <haadikhan05@gmail.com>
2024-01-20 18:32:41 -0500

    Back to Yabai, no sketchybar though
    Getting same battery as amethyst but yabai > amethyst. Sketchy was
    battery drainer

--- spacebar/util/song.sh
+++ spacebar/util/song.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+# Fetch title and artist
 TITLE="$(~/.config/spacebar/util/nowplaying-cli get title)"
 ARTIST="$(~/.config/spacebar/util/nowplaying-cli get artist)"
 
+# Check if either title or artist is null
 if [ "$TITLE" = "null" ] || [ "$ARTIST" = "null" ]; then
     echo "No Song Playing"
 else
-    echo "$TITLE - $ARTIST"
+    concatenated="$TITLE$ARTIST"
+
+    if [ ${#concatenated} -gt 70 ]; then
+        echo "Now Playing"
+    else
+        echo "$TITLE - $ARTIST"
+    fi
 fi
